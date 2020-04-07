6505e2da0559255e3d94c0f44c54848316ac7946
William Douglas <57199250+WilliamDCGomes@users.noreply.github.com>
2020-04-07 16:46:54 -0300

    Exercício 1

---
+++ Exercicio 1/Exercicio1/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio1 {
+    class Class1 {
+        public string nome1,nome2;
+        public int idade1,idade2;
+
+    }
+}
---
+++ Exercicio 1/Exercicio1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Exercicio1 {
+    class Program {
+        static void Main(string[] args) {
+            Class1 classe = new Class1();
+            Console.Write("Dados da primeira pessoa:\nNome: ");
+            classe.nome1 = Console.ReadLine();
+            Console.Write("Idade: ");
+            classe.idade1 = int.Parse(Console.ReadLine());
+            Console.Write("Dados da segunda pessoa:\nNome: ");
+            classe.nome2=Console.ReadLine();
+            Console.Write("Idade: ");
+            classe.idade2 = int.Parse(Console.ReadLine());
+            Console.Write("Pessoa mais velha: ");
+            if (classe.idade1 > classe.idade2) {
+                Console.WriteLine(classe.nome1);
+            }
+            else if (classe.idade2 > classe.idade1) {
+                Console.WriteLine(classe.nome2);
+            }
+            else {
+                Console.WriteLine(classe.nome1+" e "+classe.nome2+" tem a mesma idade!");
+            }
+        }
+    }
+}
