a96fed6b2dc7e36b4503295429a3ec9388e7b949
William Douglas <57199250+WilliamDCGomes@users.noreply.github.com>
2020-04-07 16:47:55 -0300

    Exercício 2

---
+++ Exercicio 2/Exercicio2/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Exercicio2 {
+    class Class1 {
+        public string nome1, nome2;
+        public float sal1, sal2;
+    }
+}
---
+++ Exercicio 2/Exercicio2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+namespace Exercicio2 {
+    class Program {
+        static void Main(string[] args) {
+            Class1 classe = new Class1();
+            Console.Write("Dados do primeiro funcionário:\nNome: ");
+            classe.nome1 = Console.ReadLine();
+            Console.Write("Salário: ");
+            classe.sal1= float.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Dados do segundo funcionário:\nNome: ");
+            classe.nome2 = Console.ReadLine();
+            Console.Write("Salário: ");
+            classe.sal2 = float.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine("Salário médio = "+((classe.sal1+classe.sal2)/2).ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
