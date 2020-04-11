2d63ff1275a3a19dd560cf108b43f0b8cee2f489
William Douglas <57199250+WilliamDCGomes@users.noreply.github.com>
2020-04-11 03:34:00 -0300

    Exercicio 7

---
+++ Exercicio 7/Exercicio 7/Conta.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+namespace Exercicio_7 {
+    class Conta {
+        //Atributos
+        private string _titularConta;
+        //Propriedades
+        public int NumeroConta { get; private set; }
+        public double Saldo { get; private set; }
+        //Construtores
+        public Conta(int numeroConta, string titularConta) {
+            NumeroConta = numeroConta;
+            _titularConta = titularConta;
+        }
+        public Conta(int numeroConta, string titularConta, double saldo) : this (numeroConta, titularConta) {
+            Depositar(saldo);
+        }
+        //Propriedades Customizadas
+        public string Nome {
+            get { return _titularConta; }
+            set {
+                if(value != null && value.Length > 1) {
+                    _titularConta = Nome;
+                }
+            }
+        }
+        //Métodos
+        public void Depositar(double valor) {
+            if(valor > 0) {
+                Saldo += valor;
+            }
+        }
+        public void Sacar(double valor) {
+            if (valor > 0) {
+                Saldo -= (valor + Taxa());
+            }
+        }
+        private double Taxa() {
+            return 5.00;
+        }
+        public override string ToString() {
+            return "Conta "+ NumeroConta + ", Titular: " + _titularConta + ", Saldo: $ "+ Saldo.ToString("F2",CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Exercicio 7/Exercicio 7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+namespace Exercicio_7 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Entre o número da conta: ");
+            int num_conta = int.Parse(Console.ReadLine());
+            Console.Write("Entre o títular da conta: ");
+            string titular_conta = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char verif_depo_ini = char.Parse(Console.ReadLine());
+            double dep_ini=0.00;
+            if(verif_depo_ini == 's' || verif_depo_ini == 'S') {
+                Console.Write("Entre o valor de depósito inicial: ");
+                dep_ini = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            }
+            Conta c = new Conta(num_conta, titular_conta, dep_ini);
+            Console.WriteLine("\n\nDados da conta:\n" + c+"\n");
+            Console.Write("\nEntre um valor para depósito: ");
+            c.Depositar(double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture));
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(c+"\n");
+            Console.Write("\nEntre um valor para saque: ");
+            c.Sacar(double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture));
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(c);
+        }
+    }
+}
