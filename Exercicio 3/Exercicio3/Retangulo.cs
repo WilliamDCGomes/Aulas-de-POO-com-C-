feec3a5f3ba0f06a0f0fd19af3a09f50551385dc
William Douglas <57199250+WilliamDCGomes@users.noreply.github.com>
2020-04-10 22:21:55 -0300

    Exercício 3

---
+++ Exercicio 3/Exercicio3/Retangulo.cs
@@ -0,0 +1,18 @@
+﻿using static System.Math;
+namespace Exercicio3 {
+    class Retangulo {
+        public double Largura;
+        public double Altura;
+
+        public double Area() {
+            return Largura * Altura;
+        }
+        public double Perimetro() {
+            return (Largura*2)+(Altura*2);
+        }
+        public double Diagonal() {
+            return Sqrt(Pow(Largura,2)+Pow(Altura,2));
+        }
+
+    }
+}
