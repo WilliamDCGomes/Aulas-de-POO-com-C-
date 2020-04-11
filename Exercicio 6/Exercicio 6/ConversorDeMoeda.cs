7138e815d3779bec342e7ab7555a69c10aa92c58
William Douglas <57199250+WilliamDCGomes@users.noreply.github.com>
2020-04-10 22:25:03 -0300

    Exercicio 6

---
+++ Exercicio 6/Exercicio 6/ConversorDeMoeda.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercicio_6 {
+    class ConversorDeMoeda {
+        public static double CotacaoDolar;
+        public static double QuantDolarComprar;
+        
+        private static double IOF() {
+            return CotacaoDolar * QuantDolarComprar * 0.06;
+        }
+
+        public static double ValorASerPago() {
+            return IOF() + (CotacaoDolar * QuantDolarComprar);
+        }
+    }
+}
---
+++ Exercicio 6/Exercicio 6/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+namespace Exercicio_6 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Qual é a cotação do dólar? ");
+            ConversorDeMoeda.CotacaoDolar = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? ");
+            ConversorDeMoeda.QuantDolarComprar = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Valor a ser pago em reais = " + ConversorDeMoeda.ValorASerPago().ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
