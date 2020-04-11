311c90d65dd66344f7069554dd23454befa8817c
William Douglas <57199250+WilliamDCGomes@users.noreply.github.com>
2020-04-10 22:23:12 -0300

    Exercicio 4

---
+++ Exercicio 4/Exercicio4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+namespace Exercicio4 {
+    class Program {
+        static void Main(string[] args) {
+            Funcionario f = new Funcionario();
+            Console.Write("Nome: ");
+            f.Nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            f.SalarioBruto = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            f.Imposto = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine("\nFuncionário: "+f+"\n");
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            f.AumentarSalario(double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture));
+            Console.WriteLine("\nDados atualizados: "+f);
+        }
+    }
+}
