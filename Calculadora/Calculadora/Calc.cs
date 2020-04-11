4d97a55ac585a5fb7e1cf1ca85327d4bfcba5a0f
William Douglas <57199250+WilliamDCGomes@users.noreply.github.com>
2020-04-10 22:17:12 -0300

    Calculadora

---
+++ Calculadora/Calculadora/Calc.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+namespace Calculadora {
+    class Calc {
+        public static double Pi = 3.14;
+        public static double Circuferencia(double r) {
+            return 2.0 * Pi * r;
+        }
+        public static double Volume(double r) {
+            return 4.0 / 3.0 * Pi * r * r * r; 
+        }
+    }
+}
---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+namespace Calculadora {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            double circ = Calc.Circuferencia(raio);
+            double volume = Calc.Volume(raio);
+
+            Console.WriteLine("Circuferencia: " + circ.ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Calc.Pi.ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
