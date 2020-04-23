dcd6f3888da2012b4d72e434041cc07341ef0a53
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2020-04-22 23:33:11 -0300

    Aula de Enumeracoes

---
+++ Enumerações/Enumerações/Entities/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Enumerações.Entities.Enums;
+namespace Enumerações.Entities {
+    class Order {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public Order(int id, DateTime moment, OrderStatus status) {
+            Id = id;
+            Moment = moment;
+            Status = status;
+        }
+        public Order() {
+        }
+
+        public override string ToString() {
+            return Id + ", " + Moment + ", " + Status;
+        }
+    }
+}
---
+++ Enumerações/Enumerações/Program.cs
@@ -0,0 +1,16 @@
+﻿using Enumerações.Entities;
+using Enumerações.Entities.Enums;
+using System;
+
+namespace Enumerações {
+    class Program {
+        static void Main(string[] args) {
+            Order order = new Order(1080, DateTime.Now, OrderStatus.PendingPayment);
+            Console.WriteLine(order);
+            string txt = OrderStatus.PendingPayment.ToString();
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(txt);
+            Console.WriteLine(os);
+        }
+    }
+}
