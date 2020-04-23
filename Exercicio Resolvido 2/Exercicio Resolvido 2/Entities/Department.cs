ddf667b3f0bbeaab9d7503d7662789ca659abe78
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2020-04-23 19:40:13 -0300

    Feito o segundo execicio resolvido da aula de C Sharp com POO

---
+++ Exercicio Resolvido 2/Exercicio Resolvido 2/Entities/Department.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Exercicio_Resolvido_2.Entities {
+    class Department {
+        public string Name { get; set; }
+
+        public Department() {
+        }
+        public Department(string name) {
+            Name = name;
+        }
+    }
+}
---
+++ Exercicio Resolvido 2/Exercicio Resolvido 2/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,7 @@
+﻿namespace Exercicio_Resolvido_2.Entities.Enums {
+    enum WorkerLevel : int {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
