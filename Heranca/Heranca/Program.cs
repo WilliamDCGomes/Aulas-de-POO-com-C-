2f1e844ce1db0c56f9987135f0237b043b3aa6d6
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2020-05-03 11:04:04 -0300

    Aula de Heranca

---
+++ Heranca/Heranca/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Heranca.Entities;
+namespace Heranca {
+    class Program {
+        static void Main(string[] args) {
+            Business_Account account = new Business_Account(8010, "Bob Brown", 100.0, 500.0);
+            Console.WriteLine(account.Balance);
+            //account.Balance = 600.0;
+        }
+    }
+}
