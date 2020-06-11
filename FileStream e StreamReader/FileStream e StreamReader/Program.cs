78c7a937996f4d14560d26f6c0f5f8fb7e79e8d6
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2020-06-11 16:20:17 -0300

    FileStream e ScreamReader

---
+++ FileStream e StreamReader/FileStream e StreamReader/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace FileStream_e_StreamReader {
+    class Program {
+        static void Main(string[] args) {
+            string path = @"c:\temp\file1.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+            try {
+                sr = File.OpenText(path);
+                while (!sr.EndOfStream) {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error accurred!");
+                Console.WriteLine(e.Message);
+            }
+            finally {
+                if(sr != null) {
+                    sr.Close();
+                }
+            }
+        }
+    }
+}
