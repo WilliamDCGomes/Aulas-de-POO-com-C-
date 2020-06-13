9bb416902a9214ba2a972004b8de6360d7c9cb22
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2020-06-13 18:54:26 -0300

    Path

--- Path/Path/Program.cs
+++ Path/Path/Program.cs
@@ -1,9 +1,19 @@
 ﻿using System;
+using System.IO;
+using System.Xml;
 
-namespace Path {
+namespace Paths {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            string path = @"c:\temp\myfolder\file1.txt";
+            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+            Console.WriteLine("PathSeparator: " + Path.PathSeparator);
+            Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
+            Console.WriteLine("GetFileName: " + Path.GetFileName(path));
+            Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+            Console.WriteLine("GetTempPath: " + Path.GetTempPath());
         }
     }
 }
