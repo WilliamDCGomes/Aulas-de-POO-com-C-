f1acab50cd41ecde225f046f9679b59753ca204f
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2020-08-22 13:55:34 -0300

    Aula de Generics Parte 2

--- Generics/Generics/PrintService.cs
+++ Generics/Generics/PrintService.cs
@@ -1,17 +1,17 @@
 ﻿using System;
 namespace Generics {
-    class PrintService {
-        private int[] _values = new int[10];
+    class PrintService<T> {
+        private T[] _values = new T[10];
         private int _count = 0;
 
-        public void AddValue(int value) {
+        public void AddValue(T value) {
             if (_count == 10) {
                 throw new InvalidOperationException("PrintService is full");
             }
             _values[_count] = value;
             _count++;
         }
-        public int First() {
+        public T First() {
             if (_count==0) {
                 throw new InvalidOperationException("PrintService is empty");
             }
--- Generics/Generics/Program.cs
+++ Generics/Generics/Program.cs
@@ -3,8 +3,8 @@
 namespace Generics {
     class Program {
         static void Main(string[] args) {
-            PrintService printService = new PrintService();
-            Console.WriteLine("How many values?");
+            PrintService<int> printService = new PrintService<int>();
+            Console.Write("How many values? ");
             int n = int.Parse(Console.ReadLine());
             for(int i = 0; i < n; i++) {
                 int x = int.Parse(Console.ReadLine());
