aca5d9eb4a15cf7dbbc698d89289a3a02d2d5b78
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2020-09-24 08:41:54 -0300

    Primeira parte da aula de Hash Set e Sorted Set

---
+++ HashSetESortedSet/HashSetESortedSet/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashSetESortedSet {
+    class Program {
+        static void Main(string[] args) {
+            HashSet<string> set = new HashSet<string>();
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+            Console.WriteLine(set.Contains("Notebook"));
+            foreach (String i in set) {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
