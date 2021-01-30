d388ca39271e1f56bb2f6e137a3f4d35b7c922bc
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-01-30 12:03:09 -0300

    Exercicio de Conjunto Resolvido

---
+++ Exercicio Resolvido de Conjuntos/Exercicio Resolvido de Conjuntos/Entities/LogRecord.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio_Resolvido_de_Conjuntos.Entities {
+    class LogRecord {
+        public string Username { get; set; }
+        public DateTime Instant { get; set; }
+        public int quantity = 1;
+
+        public void Increment() {
+            quantity++;
+        }
+        public override int GetHashCode() {
+            return Username.GetHashCode();
+        }
+        public override bool Equals(object obj) {
+            if(!(obj is LogRecord)) {
+                return false;
+            }
+            LogRecord other = obj as LogRecord;
+            return Username.Equals(other.Username);
+        }
+    }
+}
---
+++ Exercicio Resolvido de Conjuntos/Exercicio Resolvido de Conjuntos/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Exercicio_Resolvido_de_Conjuntos.Entities;
+
+namespace Exercicio_Resolvido_de_Conjuntos {
+    class Program {
+        static void Main(string[] args) {
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+            List<LogRecord > people = new List<LogRecord>();
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            try {
+                using (StreamReader sr = File.OpenText(path)) {
+                    while (!sr.EndOfStream) {
+                        string[] line = sr.ReadLine().Split(" ");
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        LogRecord person = new LogRecord { Username = name, Instant = instant };
+                        bool aux = set.Add(person);
+                        if (aux) {
+                            people.Add(person);
+                        } else {
+                            foreach(LogRecord i in people) {
+                                if (i.Username.Equals(person.Username)) {
+                                    i.Increment();
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                Console.WriteLine(e.Message);
+            }
+            foreach (LogRecord i in people) {
+                Console.WriteLine(i.Username + ": " + i.quantity);
+            }
+        }
+    }
+}
