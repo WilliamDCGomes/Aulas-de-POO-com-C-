e6c1f53d97f1d27cd42c5b6b9a8d8a5865adf24b
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-01-30 22:37:41 -0300

    Exercicio Proposto Dictionary

---
+++ Dictionary e SortedDictionary/Dictionary e SortedDictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary_e_SortedDictionary {
+    class Program {
+        static void Main(string[] args) {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+            cookies["user"] = "maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["phone"] = "99712234";
+            cookies["phone"] = "83737388";
+            Console.WriteLine(cookies["phone"]);
+        }
+    }
+}
