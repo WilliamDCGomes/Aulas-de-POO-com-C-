fd976fdae9978825c62c825a403a18fcb7be5947
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-02-09 13:50:30 -0300

    Predicate

---
+++ Predicate/Predicate/Program.cs
@@ -0,0 +1,22 @@
+﻿using Predicate.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Predicate {
+    class Program {
+        static void Main(string[] args) {
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+            list.RemoveAll(ProducTest);
+            foreach(Product p in list) {
+                Console.WriteLine(p);
+            }
+        }
+        public static bool ProducTest(Product p) {
+            return p.Price >= 100.0;
+        }
+    }
+}
